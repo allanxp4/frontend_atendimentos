d2b3ba23cc5b917d3e49cd9271255683f9d22497
allanxp4 <allanxp4@gmail.com>
2016-07-21 20:31:42 -0300

    modificações a página de atendimento

--- js/controllers/atendimento-controller.js
+++ js/controllers/atendimento-controller.js
@@ -7,5 +7,11 @@ app.controller('AtendimentoController', function($scope, resourceAtendimento){
         $scope.atendimentos = data;
     }, function(erro){console.log(erro)});
 
+    $scope.apagaAtendimento = function(atendimento){
+        resourceAtendimento.remove({id: atendimento.id}, function(){
+            Materialize.toast('Apagado com sucesso', 2000);
+            $scope.atendimentos.splice($scope.atendimentos.indexOf(atendimento), 1);
+        }, function(erro){console.log(erro)});
+    }
 
 });
